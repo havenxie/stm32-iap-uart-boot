d447c35a057ea70a164528568dd6fe35931b208f
havenxie <1428876581@qq.com>
2019-05-05 12:22:24 +0800

    rename project

--- MDK-ARM/RTE/_STM32-IAP_MD/RTE_Components.h
+++ MDK-ARM/RTE/_STM32-IAP_MD/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'IAP' 
+ * Project: 'IAP-UART' 
  * Target:  'STM32-IAP_MD' 
  */
 
